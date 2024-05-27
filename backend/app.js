ee3f4f612705c52406687fa53e9e527e2ed39261
Keshav <bhagatk11@gmail.com>
2024-05-27 17:19:43 +0530

    Backend error handling implemented

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,15 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 import { connectDatabase } from "./config/dbConnect.js";
+import errorMiddleware from "./middlewares/errors.js";
 
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err}`);
+    console.log(`Shutting down server due to Uncaught Exception`);
+    // Close server & exit process
+    process.exit(1);
+});
 
 dotenv.config({ path: "backend/config/config.env" });
 
@@ -16,7 +24,16 @@ import productRoutes from "./routes/products.js";
 
 app.use("/api/v1", productRoutes);
 
+app.use(errorMiddleware);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
+
+// Handle unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err}`);
+    console.log(`Shutting down server due to Unhandled Promise Rejection`)
+    // Close server & exit process
+    server.close(() => process.exit(1));
 });
