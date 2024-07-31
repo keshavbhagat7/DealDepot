fba572482d77fbdfd962bd9ed465e78a39f4b2bc
Keshav <bhagatk11@gmail.com>
2024-07-31 23:49:45 +0530

    Pagination, search, and filters added

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -4,7 +4,7 @@ import Product from '../models/product.js';
 import ErrorHandler from '../utils/errorHandler.js';
 
 // Get all products => /api/v1/products
-export const getProducts = catchAsyncErrors(async (req, res) => {
+export const getProducts = catchAsyncErrors(async (req, res, next) => {
 
     const resPerPage = 4;
     const apiFilters = new APIFilters(Product, req.query).search().filters();
